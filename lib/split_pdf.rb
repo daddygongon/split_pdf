5a5523b561c4bf4a828fed1e68d73e56e050b3bd
Shigeto R. Nishitani <bob@surface_pro.com>
2025-11-30 12:47:37 +0900

    hoge

--- lib/split_pdf.rb
+++ lib/split_pdf.rb
@@ -7,20 +7,22 @@ module SplitPdf
   class Error < StandardError; end
 
   class CLI < Thor
-    desc "puts_sample_yaml", "Prints the contents of sample.yaml"
+    desc "puts_sample_yaml", "Prints and saves a sample hc_array.yaml as sample.yaml in test_env"
     def puts_sample_yaml
+      require "yaml"
+      hc_array = {
+        source_file: './linux_basic.pdf',
+        target_dir: './linux_basic',
+        toc: [
+          {no: nil, init: 1,  fin: nil,  head: 'title'},
+          {no: 's1',init: 2,  fin: nil, head: 'command'},
+          {no: 's1',init: 7, fin: 7, head: 'line_edit'},
+        ]
+      }
+      yaml_data = YAML.dump(hc_array)
       sample_yaml_path = File.expand_path("../../test_env/sample.yaml", __dir__)
-      if File.exist?(sample_yaml_path)
-        puts File.read(sample_yaml_path)
-      else
-        puts "sample.yaml not found in test_env directory."
-        exit 1
-      end
-    end
-
-    desc "hello", "Prints Hello world."
-    def hello
-      puts "Hello world."
+      File.write(sample_yaml_path, yaml_data)
+      puts yaml_data
     end
   end
 end
